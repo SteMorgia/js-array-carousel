fe58584346ef03f50bc408e852ab174848ebd60e
Stefano Morgia <ste.morgia@gmail.com>
2022-07-08 16:17:01 +0200

    prove js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const imagesArray = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
 const items = document.querySelector('.items');
-const slideAttiva = 0;
+let slideAttiva = 0;
 
 for (let i = 0; i < imagesArray.length; i++) {
     items.innerHTML += `<div class="item"><img class="img-slide" src="${imagesArray[i]}"/></div>`
@@ -9,10 +9,23 @@ for (let i = 0; i < imagesArray.length; i++) {
 const itemList = document.getElementsByClassName('item');
 itemList[slideAttiva].classList.add ('show');
 
-const next = document.querySelector('next');
-next.addEventListener (click,
+const next = document.querySelector('.next');
+next.addEventListener("click",
     function () {
         itemList[slideAttiva].classList.remove('show');
         slideAttiva++;
         itemList[slideAttiva].classList.add('show');
-    })
+    }
+)
+if (i = 0) {
+    prev.classList.add('hide');
+}
+
+const prev = document.querySelector('.prev');
+prev.addEventListener("click",
+    function () {
+        itemList[slideAttiva].classList.remove('show');
+        slideAttiva--;
+        itemList[slideAttiva].classList.add('show');
+    }
+)
